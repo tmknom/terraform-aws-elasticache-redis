70577af25bdcab95109bf9b2c5a2e98953e3ca5b
tmknom <tmk.nom@gmail.com>
2018-12-14 20:54:52 +0900

    Implement ElastiCache Replication Group

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "elasticache_replication_group_id" {
+  value       = "${aws_elasticache_replication_group.default.id}"
+  description = "The ID of the ElastiCache Replication Group."
+}
 
+output "elasticache_replication_group_primary_endpoint_address" {
+  value       = "${aws_elasticache_replication_group.default.primary_endpoint_address}"
+  description = "The address of the endpoint for the primary node in the replication group."
+}
+
+output "elasticache_replication_group_member_clusters" {
+  value       = "${aws_elasticache_replication_group.default.member_clusters}"
+  description = "The identifiers of all the nodes that are part of this replication group."
+}
