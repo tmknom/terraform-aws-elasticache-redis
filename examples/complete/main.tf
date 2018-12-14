c35bce4dff159d46540dccf3d809e0da2b0ecd90
tmknom <tmk.nom@gmail.com>
2018-12-15 01:52:41 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,40 @@
+module "elasticache_redis" {
+  source                = "../../"
+  name                  = "example"
+  number_cache_clusters = 2
+  node_type             = "cache.m3.medium"
 
+  engine_version             = "5.0.0"
+  port                       = 56379
+  maintenance_window         = "mon:10:40-mon:11:40"
+  snapshot_window            = "09:10-10:10"
+  snapshot_retention_limit   = 1
+  automatic_failover_enabled = false
+  at_rest_encryption_enabled = false
+  transit_encryption_enabled = false
+  apply_immediately          = true
+  family                     = "redis5.0"
+  description                = "This is example"
+
+  subnet_ids          = ["${module.vpc.public_subnet_ids}"]
+  vpc_id              = "${module.vpc.vpc_id}"
+  ingress_cidr_blocks = ["${module.vpc.vpc_cidr_block}"]
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+module "vpc" {
+  source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
+  cidr_block                = "${local.cidr_block}"
+  name                      = "vpc-elasticache-redis"
+  public_subnet_cidr_blocks = ["${cidrsubnet(local.cidr_block, 8, 0)}", "${cidrsubnet(local.cidr_block, 8, 1)}"]
+  public_availability_zones = ["${data.aws_availability_zones.available.names}"]
+}
+
+locals {
+  cidr_block = "10.255.0.0/16"
+}
+
+data "aws_availability_zones" "available" {}
