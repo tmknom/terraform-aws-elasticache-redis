26094a2253e34c7a821ba657e3ac8189b7afe523
gpkfr <1516505+gpkfr@users.noreply.github.com>
2022-06-01 11:03:44 +0200

    Updated example to use the new attribute

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,8 +1,8 @@
 module "elasticache_redis" {
-  source                = "../../"
-  name                  = "example"
+  source             = "../../"
+  name               = "example"
   num_cache_clusters = 2
-  node_type             = "cache.m3.medium"
+  node_type          = "cache.m3.medium"
 
   subnet_ids         = module.vpc.public_subnet_ids
   vpc_id             = module.vpc.vpc_id
